99fe7860767a6991985eaf73902f4f691100a875
Igor_Ilin <junigos@gmail.com>
2024-04-22 03:02:27 +0200

    Removed unused variable

--- OnlineShopWeb/OnlineShopWeb/Controllers/ShoppingCartController.cs
+++ OnlineShopWeb/OnlineShopWeb/Controllers/ShoppingCartController.cs
@@ -12,7 +12,6 @@ public class ShoppingCartController : Controller
     private readonly IShoppingCartRepository _shoppingCartRepository;
     private readonly ICouponRepository _couponRepository;
     public ShoppingCartController(IShoppingCartRepository shoppingCartRepository
-        , IProductRepository productRepository
         , ICouponRepository couponRepository)
     {
         _shoppingCartRepository = shoppingCartRepository;
@@ -31,38 +30,38 @@ public class ShoppingCartController : Controller
     public IActionResult AddToShoppingCart(string product)
     {
         var productToShoppingCart = JsonSerializer.Deserialize<ProductModel>(product);
-        var dictionary = JsonSerializer.Deserialize<ShoppingCartDictionaryModel>(HttpContext.Request.Cookies["ShoppingCartDictionaryModel"]);
+        var model = JsonSerializer.Deserialize<ShoppingCartDictionaryModel>(HttpContext.Request.Cookies["ShoppingCartDictionaryModel"]);
 
-        if (dictionary.ShoppingCartModelDictionary.ContainsKey(productToShoppingCart.ProductId.Value))
+        if (model.ShoppingCartModelDictionary.ContainsKey(productToShoppingCart.ProductId.Value))
         {
-            dictionary.ShoppingCartModelDictionary[productToShoppingCart.ProductId.Value].count++;
+            model.ShoppingCartModelDictionary[productToShoppingCart.ProductId.Value].count++;
         }
         else
         {
-            dictionary.ShoppingCartModelDictionary.Add(productToShoppingCart.ProductId.Value, new ShoppingCartModel
+            model.ShoppingCartModelDictionary.Add(productToShoppingCart.ProductId.Value, new ShoppingCartModel
             {
                 ProductInCart = productToShoppingCart,
                 count = 1
             });
         }
 
-        HttpContext.Response.Cookies.Append("ShoppingCartDictionaryModel", JsonSerializer.Serialize(dictionary));
+        HttpContext.Response.Cookies.Append("ShoppingCartDictionaryModel", JsonSerializer.Serialize(model));
         return RedirectToAction("Index", "Product");
     }
 
     [HttpGet]
     public IActionResult DeleteFromShoppingCart(int key)
     {
-        var dictionary = JsonSerializer.Deserialize<ShoppingCartDictionaryModel>(HttpContext.Request.Cookies["ShoppingCartDictionaryModel"]);
+        var model = JsonSerializer.Deserialize<ShoppingCartDictionaryModel>(HttpContext.Request.Cookies["ShoppingCartDictionaryModel"]);
 
-        dictionary.ShoppingCartModelDictionary[key].count--;
+        model.ShoppingCartModelDictionary[key].count--;
 
-        if (dictionary.ShoppingCartModelDictionary[key].count == 0)
+        if (model.ShoppingCartModelDictionary[key].count == 0)
         {
-            dictionary.ShoppingCartModelDictionary.Remove(key);
+            model.ShoppingCartModelDictionary.Remove(key);
         }
 
-        HttpContext.Response.Cookies.Append("ShoppingCartDictionaryModel", JsonSerializer.Serialize(dictionary));
+        HttpContext.Response.Cookies.Append("ShoppingCartDictionaryModel", JsonSerializer.Serialize(model));
         return RedirectToAction("Index", "ShoppingCart");
     }
 
