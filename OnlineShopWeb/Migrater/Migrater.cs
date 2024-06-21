9329475e02cc9f0b92711dbf6c91c3c814811527
Igor_Ilin <junigos@gmail.com>
2024-06-21 02:54:04 +0200

    Migrator added

---
+++ OnlineShopWeb/Migrater/Migrater.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Microsoft.EntityFrameworkCore;
+//using OnlineShopWeb.Database;
+
+//namespace Migrater;
+
+//public class Migrater
+//{
+//    private readonly DbContext _dbContext;
+
+//    public Migrater(OnlineShopWebDbContext dbContext)
+//    {
+//        _dbContext = dbContext;
+//    }
+
+//    public static DbContext GetDbContext()
+//    {
+//        return _dbContext;
+//    }
+
+//    //public static DbContext GetDbContext(OnlineShopWebDbContext dbContext)
+//    //{
+//    //    return dbContext;
+//    //}
+//}
--- OnlineShopWeb/Migrater/Program.cs
+++ OnlineShopWeb/Migrater/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OnlineShopWeb.Database;
+using OnlineShopWeb.Migrations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Builder;
+using OnlineShopWeb;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+//using Migrater;
+
+var serviceCollection = new ServiceCollection();
+var config = new ConfigurationManager();
+
+config.AddJsonFile("appsettings.json");
+
+//serviceCollection.AddKeyedSingleton(serviceCollection.AddDatabase(config), "test");
+//var _dbContext = serviceProvider.GetKeyedService("test");
+
+serviceCollection.AddDatabase(config);
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+var _dbContext = serviceProvider.GetService<OnlineShopWebDbContext>();
+
+var migrator = _dbContext.GetInfrastructure().GetService<IMigrator>();
+var migrationsAssembly = _dbContext.GetInfrastructure().GetService<IMigrationsAssembly>();
+
+//migrationsAssembly
+
+migrator.Migrate();
+
+////////////////////////////////
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("The Migration was successfull");
